ff53c7b8b252b6a229c772f8f53f51e2be4ad153
Tuhin <bhowmicktuhhin34@gmail.com>
2022-02-10 00:26:58 +0530

    changes staic root

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -175,8 +175,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = path.join(BASE_DIR,'static/root')
-STATICFILES_DIRS = (path.join(BASE_DIR, 'static/files'),)
+STATIC_ROOT = path.join(BASE_DIR,'static_root')
+STATICFILES_DIRS = (path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
