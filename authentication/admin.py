76d5913cda04eabd14adc781b79d0cd0ede606c1
tuuhin <tuhinbhowmick2513@gmail.com>
2022-03-19 22:05:59 +0530

    first migration

---
+++ authentication/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Profile
+
+# registering the profile model
+admin.site.register(Profile)
---
+++ authentication/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=datetime.now)
+    # profile options will be added later
+    phoneNumber = models.BigIntegerField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
